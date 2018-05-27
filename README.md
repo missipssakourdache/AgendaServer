22cb149d375fe101f5ba8e2e03ed5af7fe7bcd82
HoussamKourdache <houssam@sensdigital.fr>
2018-05-27 12:45:52 +0200

    parametrage ok

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 ##serveur Node.JS
+
 ##installer les dépendances dans le projet AgendaServer :
+
 npm install express
 npm install mongoose
 npm install body-parser
 
-##démarrer le serveur : node app.server.js
+##démarrer le serveur : 
+
+node app.server.js
 
 ----------------------
 
@@ -12,10 +16,14 @@ npm install body-parser
 url : https://www.mongodb.com/dr/fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.6.5-signed.msi/download
 
 ##installer robo mongo : mongodb explorer
+
 url : https://robomongo.org/download
+
+https://studio3t.com/
 
 ##créer un dossier data/db
 
 ##lancer la commande pour démarrer le serveur BDD : 
+
 mongod --port 27017 --dbpath D:\Mongodb\data\db
 
--- app.server.js
+++ app.server.js
@@ -1,4 +1,76 @@
+/**
+ * importer les differents modules
+ * qui nous permettrons d'utiliser la
+ * persistence monbogdb
+ */
 var express = require('express');  
 var path = require("path");   
 var bodyParser = require('body-parser');  
 var mongo = require("mongoose"); 
+
+/**
+ * On cree notre app Express qui discutera en format 
+ * json de la BDD mongo
+ */
+var app = express()  
+app.use(bodyParser());  
+app.use(bodyParser.json({limit:'5mb'}));   
+app.use(bodyParser.urlencoded({extended:true}));  
+   
+/**
+ * on configure l'accessibilite aux webservices
+ */
+app.use(function (req, res, next) {        
+     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');    
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
+     res.setHeader('Access-Control-Allow-Credentials', true);       
+     next();  
+ });  
+
+
+var Schema = mongo.Schema;  
+  
+var UsersSchema = new Schema({      
+ name: { type: String   },       
+ address: { type: String   },   
+},{ versionKey: false });  
+   
+
+/*
+ * on demande a mongo de faire un mapping sur l'objet
+ * UsersSchema qui seront stockes dans la collections 'users'
+ */
+var model = mongo.model('users', UsersSchema, 'users');  
+
+/*
+ * home url
+ */
+app.get("/", function(){
+	console.log("Agenda is up :)");
+})
+
+/*
+ * le webservice qui permet d'enregister un utilisateur
+ */
+app.post("/api/save-user",function(req,res){   
+	var mod = new model(req.body);  
+ 	if(req.body.mode =="save"){  
+    	mod.save(function(err,data){  
+      		if(err){  
+         		res.send(err);                
+      		}else{        
+          		res.send({data:"Le nouveau utilisateur a été enregistré"});  
+      		}  
+ 		});  
+	}
+})	  
+
+
+/*
+ * On demande a notre serveur d'ecouter les 
+ * requetes qui pointent sur le port 9999 
+ */
+app.listen(9999, function () {  
+	console.log('Serveur ecoute les requetes sur le port 9999!')  
+});
